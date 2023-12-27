7836b0021cac097713863810f66fbf2c8c89a3ea
santoshkaranam <karanam@iiasa.ac.at>
2023-12-27 05:21:54 +0100

    IronPdf

---
+++ Controllers/PdfGeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire;
+using HangfireDemo.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PdfGeneratorController : ControllerBase
+{
+    [HttpGet("request", Name = "Start pdf Generation")]
+    public void Start([FromQuery] string websiteUrl)
+    {
+        BackgroundJob.Enqueue<PdfGenerationJob>(x => x.Start(websiteUrl));
+    }
+
+    [HttpGet("result", Name = "Download pdf Generation")]
+    public IActionResult WebResult()
+    {
+        var filePath = AppContext.BaseDirectory + "result.pdf";
+
+        var stream = new FileStream(filePath, FileMode.Open);
+        //return new FileStreamResult(stream, "application/pdf");   
+       // return new PhysicalFileResult(filePath, "application/pdf");
+       return new FileStreamResult(stream, "application/octet-stream") { FileDownloadName = "website.pdf" };
+    }
+}
---
+++ Core/PdfGenerationJob.cs
@@ -0,0 +1,14 @@
+﻿namespace HangfireDemo.Core;
+
+public class PdfGenerationJob
+{
+    public void Start(string website)
+    {
+        // Create a PDF from any existing web page
+        ChromePdfRenderer renderer = new ChromePdfRenderer();
+        PdfDocument pdf = renderer.RenderUrlAsPdf(website);
+
+        var filePath = AppContext.BaseDirectory + "result.pdf";
+        pdf.SaveAs(filePath);
+    }
+}
